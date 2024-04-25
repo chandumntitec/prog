c1b5568ed988c3961922ab99b504595b73eb26a6
chandumntitec <168059574+chandumntitec@users.noreply.github.com>
2024-04-25 10:34:47 +0530

    Add files via upload

---
+++ arrayex.java
@@ -0,0 +1,16 @@
+class arrayeg
+public class arrayex
+{
+public static void main(String args[])
+int[]a;
+a=new int[10];
+a[0]=10;
+a[1]=12;
+a[2]=14;
+a[3]=17;
+System.out.println("array a values are:")
+for (int i=0;i<10;i++)
+{
+System.out.println(a[i]+"/n");
+System.out.println("element at index"+i+"is:"+a[i]);
+}}
